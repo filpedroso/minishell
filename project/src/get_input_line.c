cc72da990f16fe1727233d4076f174ac9acab100
filpedroso <filpedroso3@gmail.com>
2026-03-19 01:08:17 +0000

    minisheron_$

--- project/src/get_input_line.c
+++ project/src/get_input_line.c
@@ -19,7 +19,7 @@ char	*get_input_line(t_sh *sh)
 	char	*input;
 
 	if (isatty(STDIN_FILENO))
-		input = readline("$ ");
+		input = readline("minisheron_$ ");
 	else
 		input = read_not_at_tty();
 	if (g_signal == SIGINT)
